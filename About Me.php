820bef39cd27788304cde3724b934cee14f75995
donnypm <donnypramudia04@gmail.com>
2022-03-15 12:57:14 +0700

    third commit

--- About Me.php
+++ About Me.php
@@ -16,20 +16,23 @@
 <body>
 
 
-    <nav class="navbar-expand-lg navbar navbar-dark bg-primary">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="About Me.php">About Me</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="Contact Me.php">Contact Me</a>
-                    </li>
-                </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="container-fluid" >
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="About Me.php">About Me</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="Contact Me.php">Contact Me</a>
+                </li>
+            </ul>
             </div>
         </div>
     </nav>
