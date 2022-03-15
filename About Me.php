a802f07f310b43f9d155cefb4a228f8d264ee09e
donnypm <donnypramudia04@gmail.com>
2022-03-15 13:07:01 +0700

    four commit

--- About Me.php
+++ About Me.php
@@ -16,26 +16,26 @@
 <body>
 
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-        <div class="container-fluid" >
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="About Me.php">About Me</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="Contact Me.php">Contact Me</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </nav>
+   <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="About Me.php">About Me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Contact Me.php">Contact Me</a>
+        </li>
+    </ul>
+  </div>
+  
+</nav>
 
     <br>
 
